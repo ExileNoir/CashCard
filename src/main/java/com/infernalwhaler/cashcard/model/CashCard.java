0d988bd89026fde9e10d21c7e52138b8547aa65b
Steven Deseure <deseure.s@gmail.com>
2025-05-08 19:41:32 +0200

    initial commit

---
+++ src/main/java/com/infernalwhaler/cashcard/model/CashCard.java
@@ -0,0 +1,11 @@
+package com.infernalwhaler.cashcard.model;
+
+/**
+ * @author Sdeseure
+ * @project cash card
+ * @date 8/05/2025
+ */
+
+public record CashCard(Long id, Double amount) {
+
+}
---
+++ src/test/java/com/infernalwhaler/cashcard/CashCardJsonTest.java
@@ -0,0 +1,50 @@
+package com.infernalwhaler.cashcard;
+
+import com.infernalwhaler.cashcard.model.CashCard;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Sdeseure
+ * @project cash card
+ * @date 8/05/2025
+ */
+
+@JsonTest
+public class CashCardJsonTest {
+
+    @Autowired
+    private JacksonTester<CashCard> json;
+
+    @Test
+    void cashCardSerializationTest() throws IOException {
+        final CashCard cashCard = new CashCard(99L, 123.45);
+
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        final String expected = """
+                {
+                    "id": 99,
+                    "amount": 123.45
+                }
+                """;
+        assertThat(json.parse(expected)).isEqualTo(new CashCard(1000L, 67.89));
+        assertThat(json.parseObject(expected).id()).isEqualTo(1000L);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(67.89);
+    }
+}
